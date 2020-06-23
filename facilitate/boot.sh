b7cdf71f341c222e41e24da288b22ecbdc0bd932
kitty-catt <66872501+kitty-catt@users.noreply.github.com>
2020-06-23 18:06:55 +0200

    Update boot.sh

--- facilitate/boot.sh
+++ facilitate/boot.sh
@@ -1,6 +1,6 @@
 export DOCKER_USERNAME="<DOCKER_USERNAME>"
 export DOCKER_PASSWORD="<DOCKER_PASSWORD>"
 export DOCKER_EMAIL="<DOCKER_EMAIL>"
-export BC_PROJECT="bc-2"
+export BC_PROJECT="bc-light"
 
 ./setup-bc-fundament.sh
