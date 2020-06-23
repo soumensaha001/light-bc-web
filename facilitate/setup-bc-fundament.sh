afb3eb061790a02e0095cd54babc88c3299389c9
ronald van de kuil <ronald@oc1512264167.ibm.com>
2020-06-23 08:56:07 +0200

    made a start

--- facilitate/setup-bc-fundament.sh
+++ facilitate/setup-bc-fundament.sh
@@ -1,1 +1,42 @@
-echo "hello world"
+#!/bin/sh
+
+echo "working on project ${BC_PROJECT}"
+oc project ${BC_PROJECT}
+oc status
+
+PS3='Please enter your choice: '
+options=("delete namespace" "init namespace" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "delete namespace")
+            echo "Deleting namespace ${BC_PROJECT}"
+            oc delete project ${BC_PROJECT}
+            break
+            ;;
+        "init namespace")
+            echo "Initializing namespace ${BC_PROJECT}"
+
+            # 1
+            oc new-project ${BC_PROJECT} 
+
+            # 2 - todo replace image with non-priviledged user
+            oc adm policy add-scc-to-user anyuid system:serviceaccount:${BC_PROJECT}:default
+
+            # 3 - store access token to docker hub 
+            oc create secret docker-registry regcred \
+            --docker-server=https://index.docker.io/v1/ \
+            --docker-username=${DOCKER_USERNAME} \
+            --docker-password=${DOCKER_PASSWORD} \
+            --docker-email=${DOCKER_EMAIL}
+            oc get secret regcred
+
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+echo "hello kitty catt"
