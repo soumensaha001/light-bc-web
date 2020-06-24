f23be9df92a72a6889e4704fc3c8955da9aab5f8
kitty-catt <66872501+kitty-catt@users.noreply.github.com>
2020-06-24 09:49:57 +0200

    Update boot.sh
    script application load balancer alias replacement on webhook extension.

--- facilitate/boot.sh
+++ facilitate/boot.sh
@@ -2,5 +2,6 @@ export DOCKER_USERNAME="<DOCKER_USERNAME>"
 export DOCKER_PASSWORD="<DOCKER_PASSWORD>"
 export DOCKER_EMAIL="<DOCKER_EMAIL>"
 export BC_PROJECT="bc-light"
+export APPS_LB="apps-crc.testing"
 
 ./setup-bc-fundament.sh
