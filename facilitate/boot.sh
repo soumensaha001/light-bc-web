c71441f0c052fbd04bc9ff5b409e8b2c0c925b74
Kitty Catt <66872501+kitty-catt@users.noreply.github.com>
2020-06-30 07:49:18 +0200

    Update boot.sh
    added git username

--- facilitate/boot.sh
+++ facilitate/boot.sh
@@ -1,6 +1,7 @@
 export DOCKER_USERNAME="<DOCKER_USERNAME>"
 export DOCKER_PASSWORD="<DOCKER_PASSWORD>"
 export DOCKER_EMAIL="<DOCKER_EMAIL>"
+export GIT_USERNAME="<GIT_USERNAME>"
 export BC_PROJECT="bc-light"
 export APPS_LB="apps-crc.testing"
 
