eebac7da796890fde6c3eb62dc7113e9516efce4
Kitty Catt <66872501+kitty-catt@users.noreply.github.com>
2020-08-02 12:11:07 +0200

    Update boot.sh

--- facilitate/boot.sh
+++ facilitate/boot.sh
@@ -2,9 +2,12 @@ export DOCKER_USERNAME='your docker-hub user here'
 export DOCKER_PASSWORD='your docker-hub password here'
 export DOCKER_EMAIL='your email here'
 export GIT_USERNAME='your git user here'
-export SONARQUBE_URL='URL where Sonar Qube lives happily'
-export SONARQUBE_USER='User to login to Sonar Qube'
-export SONARQUBE_PASSWORD='Password for the Sonar Qube user'
+
+# Login to Sonar Server, make a project and generate a token for it.
+export SONARQUBE_URL='http://sonarqube-sonarqube.tools.svc.cluster.local:9000'
+export SONARQUBE_PROJECT='<project here>'
+export SONARQUBE_LOGIN='<login here>'
+
 export BC_PROJECT="bc-light"
 
 ./setup-bc-fundament.sh
