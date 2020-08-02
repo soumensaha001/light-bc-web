4f809671a99a510c67aae553cabb759a81d71c4b
Kitty Catt <66872501+kitty-catt@users.noreply.github.com>
2020-08-02 12:24:59 +0200

    Update boot.sh

--- facilitate/boot.sh
+++ facilitate/boot.sh
@@ -1,13 +1,18 @@
+# Docker Hub Section
 export DOCKER_USERNAME='your docker-hub user here'
 export DOCKER_PASSWORD='your docker-hub password here'
 export DOCKER_EMAIL='your email here'
+
+# GIT Section
 export GIT_USERNAME='your git user here'
 
-# Login to Sonar Server, make a project and generate a token for it.
+# SonarQube Server Section
+# Login to SonarQube Server, make a project and generate a token for it.
 export SONARQUBE_URL='http://sonarqube-sonarqube.tools.svc.cluster.local:9000'
 export SONARQUBE_PROJECT='<project here>'
 export SONARQUBE_LOGIN='<login here>'
 
+# The OpenShift Target Namespace / Project
 export BC_PROJECT="bc-light"
 
 ./setup-bc-fundament.sh
