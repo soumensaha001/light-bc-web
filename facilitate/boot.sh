b92cdf99586524dfa0b56e02b85a2c99f9e6592e
kitty-catt <kitty-catt@example.com>
2020-08-02 09:30:06 +0200

    Make Sonar Qube URL configurable

--- facilitate/boot.sh
+++ facilitate/boot.sh
@@ -1,9 +1,10 @@
 export DOCKER_USERNAME='your docker-hub user here'
 export DOCKER_PASSWORD='your docker-hub password here'
 export DOCKER_EMAIL='your email here'
-export BC_PROJECT="bc-light"
 export GIT_USERNAME='your git user here'
-# APPS_LB - not used
-#export APPS_LB="apps-crc.testing"
+export SONARQUBE_URL='URL where Sonar Qube lives happily'
+export SONARQUBE_USER='User to login to Sonar Qube'
+export SONARQUBE_PASSWORD='Password for the Sonar Qube user'
+export BC_PROJECT="bc-light"
 
 ./setup-bc-fundament.sh
