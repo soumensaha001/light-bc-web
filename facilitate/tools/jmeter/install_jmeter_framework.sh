a63f7980f93ac03e0bfeeb3540aa76a34d3618db
kitty-catt <kitty-catt@example.com>
2020-09-01 09:50:50 +0200

    script grafana installation

--- facilitate/tools/jmeter/install_jmeter_framework.sh
+++ facilitate/tools/jmeter/install_jmeter_framework.sh
@@ -1,29 +1,29 @@
 oc project tools
 
+# Install influxdb
 oc apply -f influxdb-data.yaml 
 oc apply -f influxdb-secrets.yaml 
 oc apply -f influxdb-config.yaml 
 oc apply -f influxdb-deployment.yaml 
 oc apply -f influxdb-service.yaml 
 
-#TODO:
+oc get packagemanifests -n openshift-marketplace | grep grafana
+
+# Install Grafana
 oc apply -f - <<EOF
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
 metadata:
-  name: openshift-pipelines-operator
+  name: openshift-grafana-operator
   namespace: openshift-operators
 spec:
-  channel: ocp-4.4
-  name: openshift-pipelines-operator-rh
-  source: redhat-operators
+  channel: alpha
+  name: grafana-operator
+  source: community-operators
   sourceNamespace: openshift-marketplace
 EOF
 
-# TODO
-
-# post yaml
-# configure datasource
+# TODO: post yaml tp configure datasource
 
 #dashboard 5496 JMeter for graphana.
 #import <code>
